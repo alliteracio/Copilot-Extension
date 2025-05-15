af33eb99c2399a8d5671ef379db10c0fb983a77d
alliteracio <aleksandra.apro97@gmail.com>
2025-05-15 20:58:20 +0200

    Define Message and Payload classes

--- Copilot-Extension/Program.cs
+++ Copilot-Extension/Program.cs
@@ -10,3 +10,14 @@ app.MapGet("/info", () => "Hello Copilot!");
 app.MapGet("/callback", () => "You may close this window and return to Github where you should refresh the page and start a fresh chat.");
 
 app.Run();
+internal class Message
+{
+    public required string Role { get; set; }
+    public required string Content { get; set; }
+}
+
+internal class Payload
+{
+    public bool Stream { get; set; }
+    public required List<Message> Messages { get; set; } = [];
+}

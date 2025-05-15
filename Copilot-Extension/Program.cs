4878ebc8657e4652dfc14e23cbf43d7d4c744ada
alliteracio <aleksandra.apro97@gmail.com>
2025-05-15 20:57:02 +0200

    Add /callback endpoint to web application

--- Copilot-Extension/Program.cs
+++ Copilot-Extension/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+var appName = "szandiCopilotExtensionv1"; 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/info", () => "Hello Copilot!");
+app.MapGet("/callback", () => "You may close this window and return to Github where you should refresh the page and start a fresh chat.");
 
 app.Run();
